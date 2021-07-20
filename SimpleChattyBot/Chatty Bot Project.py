30caa8ddd8362ae3583703f442ce0cec87621e6c
AhmetCanGunay <77415040+AhmetCanGunay@users.noreply.github.com>
2021-07-20 12:29:10 +0300

    Update Chatty Bot Project.py

--- SimpleChattyBot/Chatty Bot Project.py
+++ SimpleChattyBot/Chatty Bot Project.py
@@ -1,24 +1,22 @@
 def greet(bot_name, birth_year):
-    print('Hello! My name is ' + bot_name + '.')
-    print('I was created in ' + birth_year + '.')
+    print(f'Hello! My name is {bot_name}')
+    print(f'I was created in {birth_year}')
 
 
 def remind_name():
     print('Please, remind me your name.')
     name = input()
-    print('What a great name you have, ' + name + '!')
+    print(f'What a great name you have, {name}!')
 
 
 def guess_age():
     print('Let me guess your age.')
     print('Enter remainders of dividing your age by 3, 5 and 7.')
 
-    rem3 = int(input())
-    rem5 = int(input())
-    rem7 = int(input())
+    rem3, rem5, rem7 = int(input()), int(input()), int(input())
     age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
 
-    print("Your age is " + str(age) + "; that's a good time to start programming!")
+    print(f"Your age is {age}; that's a good time to start programming!")
 
 
 def count():
